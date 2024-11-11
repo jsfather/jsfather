c93385af0a0a76b2705a059387b44958e1d42305
jsfather <keyvanmatinrad@outlook.com>
2024-11-11 20:54:21 +0330

    first data added

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,5 @@
 <template>
-  <h1>index page</h1>
+  <h1 class="text-8xl text-white">hi</h1>
+  <h1 class="text-8xl text-white">my name is Keyvan</h1>
+  <h1 class="text-8xl text-white">a frontend <code>developer</code></h1>
 </template>
