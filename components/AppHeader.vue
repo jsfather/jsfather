81e4cbad061231bf16201bb84c5fae5afbe030c8
jsfather <keyvanmatinrad@outlook.com>
2024-11-11 20:55:22 +0330

    AppHeader.vue added

---
+++ components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <nav class="h-[90px] flex flex-row content-center items-center text-white">
+    <Icon class="text-yellow-300 w-[40px] h-[69px] my-auto" name="hugeicons:java-script"/>
+    <Icon class="text-amber-800 w-[40px] h-[69px] my-auto" name="hugeicons:jupiter"/>
+    <h1 class="text-2xl">father</h1>
+<!--    <Icon class=" w-[40px] h-[40px] my-auto" name="mdi-github"/>-->
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
