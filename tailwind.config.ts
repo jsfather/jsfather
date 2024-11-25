5bebbb82db7c5321b205794966903ecc8d60f857
jsfather <keyvanmatinrad@outlook.com>
2024-11-25 23:12:55 +0330

    tailwind.config.ts added

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+module.exports = {
+  safelist: [
+    {
+      pattern:
+        /text-(red|blue|green|yellow|purple|pink|gray|indigo|teal|orange|amber)-(100|200|300|400|500|600|700|800|900)/,
+    },
+  ],
+};
