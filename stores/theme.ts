da82ae00d9959ac0f1844d4be068379a4e168ea0
jsfather <keyvanmatinrad@outlook.com>
2024-11-25 23:13:07 +0330

    theme.ts added

---
+++ stores/theme.ts
@@ -0,0 +1,16 @@
+import type { Theme } from '@/types/theme';
+
+export const useThemeStore = defineStore('themeStore', {
+  state: () => ({
+    themes: [
+      { name: 'jupiter', color: 'amber-800' },
+      { name: 'javascript', color: 'yellow-300' },
+    ] as Theme[],
+    selectedTheme: { name: 'jupiter', color: 'amber-800' } as Theme | undefined,
+  }),
+  actions: {
+    setSelectedTheme(name: string) {
+      this.selectedTheme = this.themes.find((item) => item.name === name);
+    },
+  },
+});
