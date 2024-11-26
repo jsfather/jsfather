16935585833339b287fffc7b4de18f7a00f0f8ee
jsfather <keyvanmatinrad@outlook.com>
2024-11-26 12:07:24 +0330

    hover variant added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,8 @@ module.exports = {
   safelist: [
     {
       pattern:
-        /text-(red|blue|green|yellow|purple|pink|gray|indigo|teal|orange|amber)-(100|200|300|400|500|600|700|800|900)/,
+        /(text|decoration)-(red|blue|green|yellow|purple|pink|gray|indigo|teal|orange|amber)-(100|200|300|400|500|600|700|800|900)/,
+      variants: ['hover'],
     },
   ],
 };
